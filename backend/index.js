5a9cdc38b786d6c9fbc100106a2c34080fc8142d
atharvaa9 <APARTE@STEVENS.EDU>
2024-09-16 16:53:35 -0400

    Changes

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-app.use(express.json)
-app.use(cors());
-const mainRoute = require("./routes/index");
+const rootRouter = require("./routes/index");
+
 const app = express();
 
+app.use(express.json)
+app.use(cors());
 
-app.use("/api/v1", mainRoute);
-app.listen(3000);
+app.use("/api/v1", rootRouter);
+app.get('/', (req, res) => {
+    res.json("Welcome! Connection started.");
+  });
+app.listen(3000, () => {
+    console.log("Connection started on port 3000");
+  });
 
 
 
